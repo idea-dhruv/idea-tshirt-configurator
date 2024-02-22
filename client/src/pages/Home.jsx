9e6abff5426bba75b7b3009ae8eb92b728c61017
idea-dhruv <70779749+dhruvadhia1@users.noreply.github.com>
2024-02-22 15:19:12 -0800

    Front end changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
             <img 
               src='./threejs.png'
               alt="logo"
-              className="w-8 h-8 object-contain"
+              className="w-32 h-32 object-contain"
             />
           </motion.header>
 
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,8 +5,8 @@ const state = proxy({
   color: '#EFBD48',
   isLogoTexture: true,
   isFullTexture: false,
-  logoDecal: './threejs.png',
-  fullDecal: './threejs.png',
+  logoDecal: './logo_no_bg.png',
+  fullDecal: './logo_no_bg.png',
 });
 
 export default state;
